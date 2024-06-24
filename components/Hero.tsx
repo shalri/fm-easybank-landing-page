58db67019a7bc8f4a37f1983cb673bcc71b61af3
shalri <fsaonlinework777@gmail.com>
2024-06-24 21:20:42 +0800

    Fixed mobile hero layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,19 +9,19 @@ export default function Hero() {
     // <section className="overflow-hidden bg-eb-very-light-gray pb-20 ">
     <section className="overflow-hidden bg-eb-very-light-gray pb-20 ">
       <div className="flex flex-col items-center justify-center sm:mx-auto sm:max-w-[1440px] sm:flex-row">
-        <div className="hero-bg mb-6 h-full min-w-[50dvw] sm:absolute sm:right-0 sm:top-0">
+        <div className="hero-bg mb-6 h-full sm:absolute sm:right-0 sm:top-0 sm:min-w-[50dvw]">
           <div className="relative h-full w-full px-4">
             <Image
               src="/images/image-mockups.png"
-              // width={500}
-              // height={500}
-              fill
+              width={500}
+              height={500}
+              // fill
               alt="Mockup design"
               className="-mt-[122px] object-cover "
             />
           </div>
         </div>
-        <div className="order-first max-w-[50dvw] px-6 text-center sm:text-left">
+        <div className="px-6 text-center sm:order-first sm:max-w-[50dvw] sm:text-left">
           <h1 className="mb-4 text-[2.50rem] leading-tight text-eb-dark-blue">
             {heading}
           </h1>
